63b0bbabb1ab178da0211935eca266c66ed04483
Avmar422 <notnoot584@gmail.com>
2026-09-17 18:00:07 -0400

    modificaciones archivo app.js

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/libros', librosRouter);
+app.use('/api/libros',librosRouter);
 
 app.use((req, res) => {
     res.status(404).json({ error: "Ruta no encontrada" });
